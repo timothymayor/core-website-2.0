806d05f718a2ed61476719ecbf1af6e711672b3b
hdjerry <damilolajerryhd@gmail.com>
2020-07-16 20:31:30 +0100

    Home page and dashboard

--- controller/admin.js
+++ controller/admin.js
@@ -6,25 +6,29 @@ exports.api = {
   row1 : [{ name: 'SMS',
   url:'https://sms.microapi.dev/',
   desc: 'All you need to do is have a senderID or userID. Once supplied, it would be used to identify all transactions done by you.',
-  img: 'img/email.png',
+  img: 'img/sms.png',
+  status: "verified"
 },
 {
   name: 'Transaction',
   url: 'https://transaction.microapi.dev/ui/',
   desc: 'Transaction Microservice handles transactions',
-  img: 'img/Vector (3).png',
+  img: 'img/buy.png',
+  status: "verified"
 },
 {
   name: 'Comment',
   url: 'https://comment.microapi.dev',
   desc: 'Access to built-in functionalities for when they want to implement comments and replies within their own application.',
-  img: 'img/Vector (2).png',
+  img: 'img/comment.png',
+  status: "unverified"
 },
 {
   name: 'User Management',
   url: 'https://usermanagement.microapi.dev/',
   desc: 'Actions of the User. Such actions includes :adding a user, deleting a user, updating user info etc. ',
-  img: 'img/carbon_user-multiple.png',
+  img: 'img/think.png',
+  status: "verified"
 },],
 
 row2: [
@@ -32,25 +36,29 @@ row2: [
     name: 'Notification',
     url: 'https://notification.microapi.dev',
     desc: 'This API allows you to send out notifications',
-    img: 'img/Vector (1).png',
+    img: 'img/bell.png',
+    status: "unverified"
   },
   {
     name: 'Email',
     url: 'https://email.microapi.dev/',
     desc: 'A simple service for sending emails!',
-    img: 'img/email.png',
+    img: 'img/mail.png',
+    status: "verified"
   },
   {
     name: 'Authentication',
     url: 'https://auth.microapi.dev/api/doc/',
     desc: 'A Dockerized Microservice for Authentication',
-    img: 'img/key.png',
+    img: 'img/biometric.png',
+    status: "verified"
   },
   {
     name: 'Compliant',
     url: 'https://complaint.microapi.dev/v1/docs/',
     desc: 'A micro-service for managing complaints.',
-    img: 'img/Vector (4).png',
+    img: 'img/complain.png',
+    status: "unverified"
   },
 ],
 row3: [
@@ -58,7 +66,8 @@ row3: [
     name: 'Store Management',
     url: 'https://store.microapi.dev/v1/api-docs/',
     desc: 'A Dockerized Microservice for Store Management',
-    img: 'img/settings.png',
+    img: 'img/gear.png',
+    status: "verified"
   }
 ]
 };
