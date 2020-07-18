4bd5d2269c64e477566e01b9ebb28594390a17fd
devYusuf <abdulrahmanyusuf125@gmail.com>
2020-07-18 07:36:26 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,70 @@
 const express = require('express');
 const router = express.Router();
 const auth = require("../middleware/auth");
-const {api} = require('../data/api');
-
-
-let homedatas = []
-for (let data in api) {
-  homedatas.push(...api[data]);
-}
-
+const { isEmpty } = require('validator');
+const apiModel = require("../models/api");
 
 /* GET home page. */
-router.get('/dashboard', auth, function(req, res) {
+router.get('/dashboard', auth, async function(req, res) {
+  var allApis = await apiModel.find({});
+  var approvedApis = await apiModel.find({ status: "approved" });
+  var pendingApis = await apiModel.find({ status: "pending" });
+
   res.render('pages/dashboard', {
     pageName: 'Dashbord',
-    apis: api,
-     apilist: [{
-         name: 'Barry Dick',
-         id: 'API234',
-         url: '/barry/api'
-       },
-       {
-         name: 'Ronald Taylor',
-         id: 'API244',
-         url: '/Ronald/api'
-       },
-       {
-         name: 'Jacob Hunter',
-         id: 'API245',
-         url: '/Jacob/api'
-       },
-       {
-         name: 'Juan Mitchell',
-         id: 'API255',
-         url: '/Juan/api'
-       },
-     ]
+     allApisCount: allApis.length,
+     pendingApisCount: pendingApis.length,
+     approvedApisCount: approvedApis.length,
+     allApis: allApis,
+     pendingApis: pendingApis,
+     approvedApis: approvedApis
   });
+});
+
+router.get('/approve-api', auth, async function(req, res) {
+    apiName = req.query.api;
+    await apiModel.updateOne({ name: apiName }, { status: "approved" });
+    req.flash("success", "API Approved");
+    res.redirect('/dashboard');
+});
+
+router.get('/delete-api', auth, async function(req, res) {
+    apiName = req.query.api;
+    await apiModel.deleteOne({ name: apiName });
+    req.flash("success", "API Disapproved");
+    res.redirect('/dashboard');
 });
 
 //Change this to the homepage or index
 router.get('/register-api', (req, res) => {
-
   res.render('pages/addapi', {
     pageName: 'API Form'
   })
-
-
 });
 
+router.post('/contact', (req, res) => {
+    req.flash("success", "Thanks for contacting us...");
+    res.redirect('/');
+})
 
+router.post('/register-api', (req, res) => {
+    const { owner, name, url, img, desc } = req.body;
+    let newApi = new apiModel({ owner, name, url, img, desc, status: "pending" });
+    newApi.save().then(api => {
+        req.flash("success", "API Registered");
+        res.redirect('/register-api');
+    }).catch(err => {
+    console.log(err)
+        req.flash("error", "Some error occured, please try again");
+        res.redirect('/register-api');
+    })
+})
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  var allApis = await apiModel.find({});
   res.render('pages/index', {
     pageName: 'Home',
-    apis: homedatas
+    apis: allApis
   })
 });
 
